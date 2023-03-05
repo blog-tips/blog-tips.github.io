fe10841e3c8957f2b995faea5df29eebeb1d0d53
null <hsmw@yopmail.com>
2023-03-05 10:33:42 +0800

    1

--- ad/server.js
+++ ad/server.js
@@ -1,4 +1,4 @@
-document.write('<div class="ad-t">PR</div><div class="ad"><A HREF="http://px.a8.net/svt/ejp?a8mat=16ABGT+HVV0Q+348+65MEA" target="_blank">ロリポップ！</A><img border="0" width="1" height="1" src="http://www16.a8.net/0.gif?a8mat=16ABGT+HVV0Q+348+65MEA" alt=""></div><div class="ad-txt">格安レンタルサーバー。WordPress や MovableType も。</div>');
-document.write('<div class="ad"><A href="http://px.a8.net/svt/ejp?a8mat=1HVS5E+CZZKCQ+HQW+60OXE" target="_blank">【ハッスルサーバー】</A><img border="0" width="1" height="1" src="http://www16.a8.net/0.gif?a8mat=1HVS5E+CZZKCQ+HQW+60OXE" alt=""></div><div class="ad-txt">MovableType 自動インストールあり。ブログに便利な格安サーバー</div>');
+document.write('<div class="ad-t">PR</div><div class="ad"><A HREF="http://px.a8.net/svt/ejp?a8mat=16ABGT+HVV0Q+348+65MEA" target="_blank">ロリポップ！</A></div><div class="ad-txt">格安レンタルサーバー。WordPress や MovableType も。</div>');
+document.write('<div class="ad"><A href="http://px.a8.net/svt/ejp?a8mat=1HVS5E+CZZKCQ+HQW+60OXE" target="_blank">【ハッスルサーバー】</A></div><div class="ad-txt">MovableType 自動インストールあり。ブログに便利な格安サーバー</div>');
 
 
